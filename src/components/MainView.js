39dbb61cd56cdcedd1c9a18fd170ab63de4915af
Josh Buchea <verctrl@joshbuchea.com>
2020-08-30 11:36:35 -0700

    resolve linter warnings

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -8,7 +8,37 @@ import BookmarksView from './BookmarksView';
 function useBookmarksFolder(folder) {
   const [bookmarks, setBookmarks] = useState([]);
 
+  const updateBookmarks = folder => {
+    console.log('bookmarks folder: ', folder);
+    setBookmarks(folder.children || []);
+  };
+
   useEffect(() => {
+    function getBookmarks() {
+      // Search bookmarks for folder
+      chrome.bookmarks.search(
+        {
+          url: null,
+          title: folder,
+        },
+        (results) => {
+          // console.log('bookmarks search results: ', results);
+
+          // If results, parse bookmarks
+          results.length &&
+            chrome.bookmarks.getSubTree(results[0].id, (data) =>
+              updateBookmarks(data[0])
+            );
+
+          // Fallback to Bookmarks Bar
+          !results.length &&
+            chrome.bookmarks.getSubTree("1", (data) =>
+              updateBookmarks(data[0])
+            );
+        }
+      );
+    }
+
     // Add event listeners
     if (chrome) {
       chrome.bookmarks.onCreated.addListener(getBookmarks);
@@ -31,35 +61,7 @@ function useBookmarksFolder(folder) {
         chrome.bookmarks.onChildrenReordered.removeListener(getBookmarks);
       }
     };
-  }, []);
-
-  const getBookmarks = () => {
-    // Search bookmarks for folder
-    chrome.bookmarks.search(
-      {
-        url: null,
-        title: folder
-      },
-      results => {
-        // console.log('bookmarks search results: ', results);
-
-        // If results, parse bookmarks
-        results.length &&
-          chrome.bookmarks.getSubTree(results[0].id, data =>
-            updateBookmarks(data[0])
-          );
-
-        // Fallback to Bookmarks Bar
-        !results.length &&
-          chrome.bookmarks.getSubTree('1', data => updateBookmarks(data[0]));
-      }
-    );
-  };
-
-  const updateBookmarks = folder => {
-    console.log('bookmarks folder: ', folder);
-    setBookmarks(folder.children || []);
-  };
+  }, [folder]);
 
   return bookmarks;
 }

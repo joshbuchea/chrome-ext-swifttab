0a19cb1d315692aea9bebbc513f94ab9e61ca080
Josh Buchea <verctrl@joshbuchea.com>
2022-12-27 19:47:30 -1000

    add firefox favicon url scheme

--- src/components/BookmarksView.js
+++ src/components/BookmarksView.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Masonry from 'react-masonry-component';
 
+// Firefox 1.0+ (tested on Firefox 45 - 53)
+const isFirefox = typeof InstallTrigger !== 'undefined';
+
 const masonryOptions = {
   // fitWidth: true,
   stagger: 30,
@@ -8,6 +11,11 @@ const masonryOptions = {
 };
 
 function getImageString(url) {
+  if (isFirefox) {
+    // favicon URL scheme for Firefox
+    return `image-set(url("https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=16") 1x, url("https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=32") 2x)`;
+  }
+
   // chrome-extension://<id>/_favicon/?pageUrl=https://example.com&size=24
   // Uncomment the code below when it's time to upgrade to extension manifest v3
   // return `-webkit-image-set(
@@ -19,10 +27,11 @@ function getImageString(url) {
   //   }/_favicon/?pageUrl=${encodeURIComponent(url)}&size=32') 2x
   // )`;
 
+  // favicon URL scheme for Chrome manifest v2
   return `-webkit-image-set(
-    url('chrome://favicon/size/16@1x/${url}') 1x,
-    url('chrome://favicon/size/16@2x/${url}') 2x
-  )`;
+      url('chrome://favicon/size/16@1x/${url}') 1x,
+      url('chrome://favicon/size/16@2x/${url}') 2x
+    )`;
 }
 
 function BookmarksView(props) {

dd26a596a6a2e2635685a28e72479f3740f5e4c2
Josh Buchea <verctrl@joshbuchea.com>
2022-12-31 15:02:44 -1000

    hide toolbar buttons on firefox

--- src/components/AppToolbar.js
+++ src/components/AppToolbar.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ToolbarIconButton from './ToolbarIconButton';
 
+// Firefox 1.0+ (tested on Firefox 45 - 53)
+// TODO: replace InstallTrigger, it is deprecated and will be removed in the future
+const isFirefox = typeof InstallTrigger !== 'undefined';
+
 function AppToolbar(props) {
   const { handleSearchChange, searchValue } = props;
   const bookmarksButtonLabel =
@@ -23,26 +27,30 @@ function AppToolbar(props) {
       </div>
 
       <div>
-        <ToolbarIconButton
-          icon="star"
-          title="Bookmarks"
-          url="chrome://bookmarks/"
-        />
-        {/* <ToolbarIconButton
-          icon="tab"
-          title="Tabs"
-          url="chrome://history/syncedTabs"
-        /> */}
-        <ToolbarIconButton
-          icon="history"
-          title="History"
-          url="chrome://history"
-        />
-        <ToolbarIconButton
-          icon="file_download"
-          title="Downloads"
-          url="chrome://downloads/"
-        />
+        {!isFirefox && (
+          <>
+            <ToolbarIconButton
+              icon="star"
+              title="Bookmarks"
+              url="chrome://bookmarks/"
+            />
+            {/* <ToolbarIconButton
+              icon="tab"
+              title="Tabs"
+              url="chrome://history/syncedTabs"
+            /> */}
+            <ToolbarIconButton
+              icon="history"
+              title="History"
+              url="chrome://history"
+            />
+            <ToolbarIconButton
+              icon="file_download"
+              title="Downloads"
+              url="chrome://downloads/"
+            />
+          </>
+        )}
       </div>
     </div>
   );
